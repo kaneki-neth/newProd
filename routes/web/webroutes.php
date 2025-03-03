79ad99ef31b30d01095a9836a022c61a37d9f9b2
davepasco <davepasco.msun@gmail.com>
2025-03-03 18:06:16 +0800

    adding files

--- routes/web/webroutes.php
+++ routes/web/webroutes.php
@@ -1,6 +1,10 @@
 <?php
 
 use App\Http\Controllers\web\home;
+use App\Http\Controllers\web\aboutus;
+use App\Http\Controllers\web\archive;
+use App\Http\Controllers\web\contactus;
+use App\Http\Controllers\web\newsevent;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [home::class, 'index']);

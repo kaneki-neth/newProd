b4a1a53682f371c1b7364c4a60f56ca832234425
Kprjjl <69335092+Kprjjl@users.noreply.github.com>
2025-03-07 15:09:45 +0800

    updated web news_event index page

--- app/Http/Controllers/web/newsevent.php
+++ app/Http/Controllers/web/newsevent.php
@@ -3,28 +3,80 @@
 namespace App\Http\Controllers\web;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use DB;
 
 class newsevent extends Controller
 {
     //
-    public function index(){
-        return view('web.news_and_events.events');
+    public function index()
+    {
+        $news = DB::table('news_events')
+            ->select('ne_id', 'image_file', 'title', 'date', 'description')
+            ->where('category', 'news')
+            ->where('enabled', 1)
+            ->where('date', '<=', date('Y-m-d H:i:s'))
+            ->orderBy('date', 'desc')
+            ->get()
+            ->toArray();
+
+        foreach ($news as $item) {
+            $date = date_create($item->date);
+            $item->date = date_format($date, 'j M Y');
+            $item->excerpt = $this->generate_excerpt($item->description);
+            unset($item->description);
+        }
+
+        $events = DB::table('news_events')
+            ->select('ne_id', 'image_file', 'title', 'date', 'description')
+            ->where('category', 'event')
+            ->where('enabled', 1)
+            // ->where('date', '<=', date('Y-m-d H:i:s'))
+            ->orderBy('date', 'desc')
+            ->get()
+            ->toArray();
+
+        foreach ($events as $item) {
+            $date = date_create($item->date);
+            $item->date = date_format($date, 'l F j Y');
+            $item->time = date_format($date, 'h:i A');
+            $item->excerpt = $this->generate_excerpt($item->description);
+            unset($item->description);
+        }
+
+        return view('web.news_and_events.events', compact('news', 'events'));
     }
 
-    public function events_news_content(){
+    public function generate_excerpt($description, $maxLength = 300)
+    {
+        $img_replacement = ' ';
+        $img_pattern = '/<img[^>]+src="data:image\/[^;]+;base64,[^"]+"[^>]*>/';
+        $cleanDescription = preg_replace($img_pattern, $img_replacement, $description);
+        $cleanDescription = $description;
+        $cleanDescription = strip_tags($cleanDescription);
+        if (strlen($cleanDescription) > $maxLength) {
+            return substr($cleanDescription, 0, $maxLength).'...';
+        }
+
+        return $cleanDescription;
+    }
+
+    public function events_news_content()
+    {
         return view('web.news_and_events.articles.events_news_content');
     }
 
-    public function events_research_content(){
+    public function events_research_content()
+    {
         return view('web.news_and_events.articles.events_research_content');
     }
 
-    public function events_blog_content(){
+    public function events_blog_content()
+    {
         return view('web.news_and_events.articles.events_blog_content');
     }
 
-    public function events_events_content(){
+    public function events_events_content()
+    {
         return view('web.news_and_events.articles.events_events_content');
     }
 }

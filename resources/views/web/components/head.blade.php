d79affd62a8db5cf61f49ef5279fa0f3b7720208
Kenneth Jay Lumahang <kaneki.neth@gmail.com>
2025-03-07 11:20:37 +0800

    erge latest main, created video seeder

--- resources/views/web/components/head.blade.php
+++ resources/views/web/components/head.blade.php
@@ -16,7 +16,8 @@
 <link href="web/assets/css/main.css" rel="stylesheet">
 
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css">
+<link rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css">
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>

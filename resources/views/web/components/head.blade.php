a04317397d7451026da47d38abf9116a8e0b7872
Kprjjl <69335092+Kprjjl@users.noreply.github.com>
2025-03-10 11:04:41 +0800

    updated web scripts and links

--- resources/views/web/components/head.blade.php
+++ resources/views/web/components/head.blade.php
@@ -4,16 +4,16 @@
 <meta name="description" content="">
 <meta name="keywords" content="">
 
-<link href="web/assets/img/matix_logo_white.png" rel="icon">
-<link href="web/assets/img/matix_logo_white.png" rel="apple-touch-icon">
+<link href="/web/assets/img/matix_logo_white.png" rel="icon">
+<link href="/web/assets/img/matix_logo_white.png" rel="apple-touch-icon">
 
-<link href="web/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-<link href="web/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-<link href="web/assets/vendor/aos/aos.css" rel="stylesheet">
-<link href="web/assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
-<link href="web/assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+<link href="/web/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+<link href="/web/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+<link href="/web/assets/vendor/aos/aos.css" rel="stylesheet">
+<link href="/web/assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+<link href="/web/assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
 
-<link href="web/assets/css/main.css" rel="stylesheet">
+<link href="/web/assets/css/main.css" rel="stylesheet">
 
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css">
--- resources/views/web/layout/default.blade.php
+++ resources/views/web/layout/default.blade.php
@@ -20,15 +20,15 @@
     <div id="preloader"></div>
 
     <!-- Vendor JS Files -->
-    <script src="web/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script src="web/assets/vendor/php-email-form/validate.js"></script>
-    <script src="web/assets/vendor/aos/aos.js"></script>
-    <script src="web/assets/vendor/glightbox/js/glightbox.min.js"></script>
-    <script src="web/assets/vendor/purecounter/purecounter_vanilla.js"></script>
-    <script src="web/assets/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="/web/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="/web/assets/vendor/php-email-form/validate.js"></script>
+    <script src="/web/assets/vendor/aos/aos.js"></script>
+    <script src="/web/assets/vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="/web/assets/vendor/purecounter/purecounter_vanilla.js"></script>
+    <script src="/web/assets/vendor/swiper/swiper-bundle.min.js"></script>
 
     <!-- Main JS File -->
-    <script src="web/assets/js/main.js"></script>
+    <script src="/web/assets/js/main.js"></script>
 </body>
 
 </html>

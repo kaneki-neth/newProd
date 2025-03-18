14b8379d832b628e9ecf5fdb14dac2c6961b29d0
Sophia Jade Esclares <sophiajade.esclares@g.msuiit.edu.ph>
2025-03-18 10:01:04 +0800

    Remove .DS_Store files and add to .gitignore

--- resources/views/web/news_and_events/articles/events_research_content.blade.php
+++ resources/views/web/news_and_events/articles/events_research_content.blade.php
@@ -1,9 +1,9 @@
 @extends('web.layout.default')
 
 @section('content')
-  <main class="main-news">
+<main class="main-news">
 
-    <div class="article-container">
+  <div class="article-container">
     <a href="/events?category=research">
       <div class="article-category">Research</div>
     </a>
@@ -11,34 +11,35 @@
       {{ $research->title }}
     </h1>
     <div class="article-meta">
-      <p class="article-date">Posted <span>{{ $research->date }}</span></p>
+      <p class="article-date">Published <span>{{ $research->date }}</span></p>
       <p class="article-author">by <span>{{ implode(', ', $authors) }}</span></p>
     </div>
     <img src="{{ asset('storage/' . $research->image_file) }}" alt="{{ $research->title }}"
       class="article-featured-image" />
 
-    <div class="article-content">
+    <div class="generate-qr" data-bs-toggle="modal" data-bs-target="#qrModal">
+      <i class="bi bi-filetype-pdf"></i>
       @foreach($files as $file)
-      <li class="file-display d-flex align-items-center mb-2">
       <a href="" onclick="openPdf('{{ asset('storage/' . $file->file_path) }}')">
-      <i class="fa-solid fa-file-pdf"></i>
-      <span>View or download PDF</span>
+        <span>View or download PDF</span>
       </a>
-      </li>
-    @endforeach
+      @endforeach
+    </div>
+
+    <div class="article-content">
       <p>
-      <?php echo $research->description; ?>
+        <?php echo $research->description; ?>
       </p>
     </div>
-    </div>
-  </main>
+  </div>
+</main>
 
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-  <script>
-    $("#nav-events").addClass("active");
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+<script>
+  $("#nav-events").addClass("active");
 
-    function openPdf(pdfUrl) {
+  function openPdf(pdfUrl) {
     window.open(pdfUrl, '_blank');
-    }
-  </script>
+  }
+</script>
 @endsection
